0bdfd6e4e25a60a280b06193e916f211d4736d87
kanoqwq <1540508428@qq.com>
2023-04-19 21:58:31 +0800

    添加live2d相关store

--- src/store/Configs/index.ts
+++ src/store/Configs/index.ts
@@ -1,22 +1,27 @@
 import { defineStore } from "pinia";
-import { getItemArray, setItem } from "@/utils/storage";
+import { getItemArray, setItem, getItem } from "@/utils/storage";
 export const Configs = defineStore("Configs", {
   state() {
     return {
       //背景图片（多图）
       backgroundImages: getItemArray("backgroundImages") || [],
+      //是否启用live2d
+      live2dIsEnabled: getItem("live2dEnabled") == 'true' ? true : false
     };
   },
   getters: {
     bgs(state) {
       return state.backgroundImages;
     },
+    live2dEnabled(state) {
+      return state.live2dIsEnabled
+    }
   },
   actions: {
     //设置背景图片
     setBackgroundImage(image: string) {
       //去重
-      //   let index = this.backgroundImages.indexOf(image);
+      //   let index = this.backgroundImages.indexOf(image); 
       //   if (index == -1) {
       if (image) {
         this.backgroundImages.push(image);
@@ -38,5 +43,10 @@ export const Configs = defineStore("Configs", {
     resetBackground() {
       localStorage.removeItem("backgroundImages");
     },
+    //开关live2d功能
+    toggleLive2d(flag: boolean) {
+      this.live2dIsEnabled = flag
+      setItem("live2dEnabled", flag);
+    }
   },
 });
